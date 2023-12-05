27a74be72558b1facb22584587520c3e9d0d4bb7
Melvin SILVA <melvin.silva@sncf.fr>
2023-12-05 17:00:14 +0100

    test endpoint tips

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, HostBinding, effect, signal } from '@angular/core';
 import { HeaderComponent } from './components/header.component';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home.component';
+import { CarteComponent } from './components/tips/card.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,16 @@ import { HomeComponent } from './components/home.component';
   template: `
     <app-header></app-header>
     <app-home></app-home>
+    <app-cartes></app-cartes>
     <router-outlet></router-outlet>
   `,
   styles: [``],
-  imports: [HomeComponent, RouterOutlet, HeaderComponent],
+  imports: [
+    HomeComponent,
+    RouterOutlet,
+    HeaderComponent,
+    CarteComponent,
+    HttpClientModule,
+  ],
 })
 export class AppComponent {}
