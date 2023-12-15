1005da91c98c6833c0299e6d51a009b8e979413c
Melvin SILVA <melvin.silva@sncf.fr>
2023-12-15 02:27:40 +0100

    test

--- frontend/src/app/components/header.component.ts
+++ frontend/src/app/components/header.component.ts
@@ -22,7 +22,7 @@ import { RouterLink } from '@angular/router';
                 class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
                 href="/"
               >
-                Accueil
+                Accueil2
               </a>
             </li>
 
